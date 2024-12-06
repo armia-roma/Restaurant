a929b286b7dfd7019f74fc452d78b0f5b5bdace4
armia-roma <armia_bos@gmail.com>
2024-12-06 11:41:03 +0400

    add: custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  theme: {
+    colors:{
+      'indigo':"#312e81"
+    } 
+  },
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
