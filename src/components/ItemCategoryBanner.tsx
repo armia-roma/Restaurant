b624fa30ed531d9cf45a461fca8f2f7b9666773a
armia-roma <armia_bos@gmail.com>
2024-12-07 22:13:34 +0400

    rename category banner

--- src/components/ItemCategoryBanner.tsx
+++ src/components/ItemCategoryBanner.tsx
@@ -1,19 +1,19 @@
 interface Props {
 	title: string;
 	isSelect: boolean;
-	categorieId: string | number;
-	onClick: (categorieId: string | number) => void;
+	categoryId: string | number;
+	onClick: (categoryId: string | number) => void;
 }
 
-export default function ItemCategorieBanar({
+export default function ItemCategoryBanner({
 	title,
 	isSelect,
+	categoryId,
 	onClick,
-	categorieId,
 }: Props) {
 	return (
 		<div
-			onClick={() => onClick(categorieId)}
+			onClick={() => onClick(categoryId)}
 			className={`flex-none p-2 w-3/4 md:w-1/3 lg:w-1/4 text-center rounded border-2 border-blue-900 ${
 				isSelect ? "bg-blue-900 text-white" : "bg-white text-blue-900 "
 			}`}
