e808748b722c43802d876962de4bc467b13421c9
armia-roma <armia_bos@gmail.com>
2024-12-07 10:56:21 +0400

    add React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import CategoriesListPage from "./pages/CategoriesListPage.tsx";
+import ItemListPage from "./pages/ItemListPage.tsx";
+const router = createBrowserRouter([
+	{path: "/items", element: <ItemListPage />},
+	{path: "/categories", element: <CategoriesListPage />},
+]);
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
