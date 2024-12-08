b8ecc5ab6ebe25fc371ee5224b779b51d1349e0c
armia-roma <armia_bos@gmail.com>
2024-12-08 23:57:04 +0400

    rename select to is selected

--- src/components/ItemCategoryBanner.tsx
+++ src/components/ItemCategoryBanner.tsx
@@ -1,13 +1,13 @@
 interface Props {
 	title: string;
-	isSelect: boolean;
+	isSelected: boolean;
 	categoryId: string | number;
 	onClick: (categoryId: string | number) => void;
 }
 
 export default function ItemCategoryBanner({
 	title,
-	isSelect,
+	isSelected,
 	categoryId,
 	onClick,
 }: Props) {
@@ -15,7 +15,9 @@ export default function ItemCategoryBanner({
 		<div
 			onClick={() => onClick(categoryId)}
 			className={`flex-none p-2 w-3/4 md:w-1/3 lg:w-1/4 text-center rounded border-2 border-blue-900 ${
-				isSelect ? "bg-blue-900 text-white" : "bg-white text-blue-900 "
+				isSelected
+					? "bg-blue-900 text-white"
+					: "bg-white text-blue-900 "
 			}`}
 		>
 			{title}
