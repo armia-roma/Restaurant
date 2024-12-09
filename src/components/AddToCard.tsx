a252019c949d60f1a22efb730e417b4dad05a140
armia-roma <armia_bos@gmail.com>
2024-12-10 03:37:39 +0400

    Refactor  AddtoCard  to read dynamic data

--- src/components/AddToCard.tsx
+++ src/components/AddToCard.tsx
@@ -1,19 +1,21 @@
+import {Item} from "../pages/ItemListPage";
 interface Props {
-	AddToCard: () => void;
+	totalPrice: number;
+	addToCard: () => void;
 }
-export default function AddToCard({AddToCard}: Props) {
+export default function AddToCard({totalPrice, addToCard}: Props) {
 	return (
-		<div className="flex justify-between items-center px-4 mt-2 bg-blue-900 rounded-xl cursor-pointer">
-			<div
-				className="flex justify-center align-center space-x-4 p-2"
-				onClick={() => AddToCard()}
-			>
+		<div
+			className="flex justify-between items-center px-4 mt-2 bg-blue-900 rounded-xl cursor-pointer"
+			onClick={() => addToCard()}
+		>
+			<div className="flex justify-center align-center space-x-4 p-2">
 				<div className="bg-gray-200 text-blue-900 flex justify-center align-center w-4 text-center rounded-sm">
 					+
 				</div>
 				<p className="text-md text-white">Add to Cart</p>
 			</div>
-			<div className="text-white">AED 234</div>
+			<div className="text-white">{totalPrice} AED</div>
 		</div>
 	);
 }
