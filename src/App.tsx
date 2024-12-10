50f9e5a7a206778a00bd4cbf09b6e0320ab88671
armia-roma <armia_bos@gmail.com>
2024-12-10 10:48:15 +0400

    Enhance UI with improved styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import {Link, Outlet} from "react-router-dom";
 import Nav from "./components/Nav";
-
+import {useState} from "react";
 function App() {
 	return (
 		<>
 			<Nav></Nav>
 			<Link
 				to="/categories"
-				className="flex items-center justify-center text-white mt-10 hover:translate-x-1"
+				className="flex items-center justify-center text-white mt-10 "
 			>
-				<div className="w-1/2 h-48 bg-blue-900 flex items-center justify-center text-white rounded cursor-pointer">
+				<div className="w-1/2 h-48 bg-blue-900 flex items-center justify-center text-white rounded cursor-pointer hover:bg-gray-200 transition-transform duration-300 ease-out rounded-lg shadow-lg transform hover:scale-105">
 					<h1 className="text-4xl md:text-6xl font-bold">
 						Categories
 					</h1>
